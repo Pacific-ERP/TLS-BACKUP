f18096211d68888ac5f9373511264bb47994a8b7
Tepava <mehdi@pacific-erp.com>
2022-03-31 23:46:51 +0000

    [ADD] pe_account_move : clôture mensuel 10 du mois

---
+++ pe_account_move/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Custom module Comptabilité (PE)",
+    "summary": "Modif non studio (PE)",
+    "version": "0.1",
+    "category": "Pacific-ERP",
+    "author": "Mehdi Tepava",
+    'website': "https://www.pacific-erp.com/",
+    "depends": ['account'],
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "data": [
+        
+        ],
+}
