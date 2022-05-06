14b9e83108fb3efc9f324b05b508723b5500c120
Tepava <mehdi@pacific-erp.com>
2022-05-06 00:36:05 +0000

    [ADD] pe_crm : Retirer le total revenu espérer de la progress bar du module CRM en vue Kanban et remplace par le total des enregistrement par colonne

---
+++ pe_crm/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Customisation module CRM (Pacifi-ERP)",
+    "summary": "Custom modules regroupant les customisations apporté au modules CRM",
+    "version": "15.0.0.0.1",
+    "category": "Pacific-ERP",
+    "author": "Mehdi Tepava",
+    'website': "https://www.pacific-erp.com/",
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["crm"],
+    "data": [
+        "views/crm_case_kanban_view_leads.xml"
+        ],
+}
