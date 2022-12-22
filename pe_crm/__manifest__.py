1e919a183ba462ebfc752ac6ff8d2bd2ef545d7e
Tepava <mehdi@pacific-erp.com>
2022-12-22 02:31:43 +0000

    [UPDATE] pe_crm v15.0.0.0.5: Ajout notification au passage en opportunité des pistes

--- pe_crm/__manifest__.py
+++ pe_crm/__manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "Customisation module CRM (Pacifi-ERP)",
     "summary": "Custom modules regroupant les customisations apporté au modules CRM",
-    "version": "15.0.0.0.4",
+    "version": "15.0.0.0.5",
     "category": "Pacific-ERP",
     "author": "Mehdi Tepava",
     'website': "https://www.pacific-erp.com/",
