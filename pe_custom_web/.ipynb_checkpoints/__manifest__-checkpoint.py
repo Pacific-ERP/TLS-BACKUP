ee1a0f4a78ed681a14062ed602c19ae1286331f3
Tepava <mehdi@pacific-erp.com>
2022-12-22 21:12:25 +0000

    [UPDATE] pe_custom_web v0.0.3: Ajout des champs réf client et lieu de livraions plus status de l'opportunité sur le formulaire devis

--- pe_custom_web/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ pe_custom_web/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -5,9 +5,10 @@
     'description': """
         Listing:
             - Partie portail clients (devis, bon de commande) ajouter les totaux
-            - Partie 
+            - Partie portail client dans le formulaire des devis ajouter les champs (référence client, lieu de livraison)
+            
     """,
-    "version": "0.0.1",
+    "version": "0.0.3",
     "category": "Uncategorized",
     "author": "Mehdi Tepava",
     'website': "https://www.pacific-erp.com/",
@@ -20,5 +21,6 @@
         "report/portal_my_quotations.xml",
         "report/portal_my_opportunity.xml",
         "report/portal_my_opportunities.xml",
+        "report/sale_order_portal_template.xml",
         ],
 }
