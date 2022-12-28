bf64d071a2517502768a83c8f1ebee1b28823477
Tepava <mehdi@pacific-erp.com>
2022-12-28 19:49:09 +0000

    [update] pe_custom_web

--- pe_custom_web/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ pe_custom_web/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -8,7 +8,7 @@
             - Partie portail client dans le formulaire des devis ajouter les champs (référence client, lieu de livraison)
             
     """,
-    "version": "0.0.3",
+    "version": "0.0.4",
     "category": "Uncategorized",
     "author": "Mehdi Tepava",
     'website': "https://www.pacific-erp.com/",
@@ -23,6 +23,7 @@
         "report/portal_my_opportunity.xml",
         "report/portal_my_opportunities.xml",
         "report/products_item.xml",
+        "report/product_price.xml",
         "report/sale_order_portal_template.xml",
         ],
 }
